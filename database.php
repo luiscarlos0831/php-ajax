906595d091441fb6a08868730a65d04308280933
luiscarlos <luiscarlos_0831@hotmail.com>
2019-03-02 17:54:22 -0500

    tutorial php ajax finished

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+
+  $connection = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'tasks-app'
+  );
+
+  // for testing connection
+  # if($connection){
+  #   echo "Database is connected";
+  # }
+
+?>
